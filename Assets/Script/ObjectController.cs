495fa2317a4240519a52a95b9eabc26134d819b9
Alexandregaudencio <alexandre.gtpjr@gmail.com>
2023-01-08 09:34:08 -0300

    Update physic Object

---
+++ Assets/Script/ObjectController.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class ObjectController : MonoBehaviour
+{
+
+    [SerializeField][Range(0, 10)] private int speed = 2;
+    private Vector3 direction;
+    private SphereCollider sphereCollider;
+    public Bounds bound => sphereCollider.bounds;
+    private float radius => sphereCollider.radius;
+
+    private void Awake()
+    {
+        sphereCollider = gameObject.GetComponent<SphereCollider>();
+
+    }
+    private void Start()
+    {
+        //randomize direction on start
+        direction = new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), Random.Range(-1f, 1f));
+
+    }
+    private void Update()
+    {
+        OutsideBoundary(transform.position);
+        transform.position += direction * speed * Time.deltaTime;
+
+    }
+
+
+    private void OutsideBoundary(Vector3 targetPosition)
+    {
+        Vector3 boundarySimulationVector = Vector3.one * GameManager.instance.worldBoundarySize;
+        if (targetPosition.x + radius > boundarySimulationVector.x) direction.x *= -1;
+        if (targetPosition.y + radius > boundarySimulationVector.y) direction.y *= -1;
+        if (targetPosition.z + radius > boundarySimulationVector.z) direction.z *= -1;
+        if (targetPosition.x + -radius < -boundarySimulationVector.x) direction.x *= -1;
+        if (targetPosition.y + -radius < -boundarySimulationVector.y) direction.y *= -1;
+        if (targetPosition.z + -radius < -boundarySimulationVector.z) direction.z *= -1;
+    }
+
+    public void ChangeDirection(Vector3 target)
+    {
+        direction = target;
+    }
+
+}
